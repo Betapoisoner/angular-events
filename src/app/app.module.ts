58aaec73530400b79de053704dddfa02629d622b
BetaPoisoner <sdhds954@gmail.com>
2024-10-08 13:53:46 +0200

    Se ha añadido la busqueda y creacion de ecventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EventosShowComponent } from './eventos-show/eventos-show.component';
+import { EventoFilterPipe } from './pipes/evento-filter.pipe';
+import { FormsModule } from '@angular/forms';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EventosShowComponent
-  ],
-  imports: [
-    BrowserModule,
-  ],
+  declarations: [AppComponent, EventosShowComponent, EventoFilterPipe],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/eventos-show/eventos-show.component.ts
+++ src/app/eventos-show/eventos-show.component.ts
@@ -7,20 +7,49 @@ import { Evento } from '../interfaces/evento';
   styleUrls: ['./eventos-show.component.css'],
 })
 export class EventosShowComponent {
+  search: any = null;
+  newEvento: Evento = {
+    title: '',
+    description: '',
+    image: '',
+    price: 0,
+    date: '',
+  };
   events: Evento[] = [
     {
       title: 'Evento de prueva',
-      image: '',
+      image: 'assets/evento1.jpg',
       date: '2019-03-15',
       description: 'Nos lo pasaremos genial',
       price: 23.95,
     },
     {
       title: 'Evento de prueva 2',
-      image: '',
+      image: 'assets/evento2.jpg',
       date: '2019-03-21',
       description: 'Este es peor',
       price: 15.5,
     },
   ];
+  orderDate() {
+    this.search = null;
+    this.events = this.events.sort(
+      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+  }
+  orderPrice() {
+    this.search = null;
+    this.events = this.events.sort((a, b) => a.price - b.price);
+  }
+  changeImage(fileInput: HTMLInputElement) {
+    if (!fileInput.files || fileInput.files.length === 0) {
+      return;
+    }
+    const reader: FileReader = new FileReader();
+    reader.readAsDataURL(fileInput.files[0]);
+    reader.addEventListener('loadend', (e) => {
+      this.newEvento.image = reader.result as string;
+    });
+  }
+  addEvento() {}
 }
